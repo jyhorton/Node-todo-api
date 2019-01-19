f80209dd805759e99e9f99511f92a249fd6b73be
jyhorton <jaydenhorton2@gmail.com>
2019-01-19 03:11:56 -0800

    Add GET method

--- server/server.js
+++ server/server.js
@@ -21,10 +21,20 @@ app.post( '/todos', (req, res) => {
 });
 
 
+app.get('/todos', (req, res) => {
+	Todo.find().then((todos) => {
+		res.send({todos});
+	}, (e) => {
+		res.status(400).send(e);
+	});	
+});
+
 
 app.listen(8000, () => {
 	console.log('Started on port 8000');
 });
+
+//github.com/jyhorton/Node-todo-api.git
 
 
 
