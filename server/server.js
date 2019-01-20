d6ffaece2b06cd4f62f9f4e2019e41e36b6e4b13
jyhorton <jaydenhorton2@gmail.com>
2019-01-20 03:13:02 -0800

    add GET id

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var bodyParser = require('body-parser');
+var {ObjectID} = require('mongodb');
 
 var {mongoose} = require('./db/mongoose');
 var {Todo} = require('./models/todo');
@@ -29,6 +30,27 @@ app.get('/todos', (req, res) => {
 	});	
 });
 
+//http://localhost:8000/todos/5c43056f6935f4130860790a
+app.get('/todos/:id', (req,res) => {
+	var id = req.params.id;
+		
+	if (!ObjectID.isValid(id)) {
+		console.log('Not Valid');
+		return res.status(404).send();
+	}
+	
+	Todo.findById(id).then((todo) => {
+		if (!todo) {
+			console.log('Not Found');
+			return res.status(404).send();
+		}
+		
+		res.send({todo});
+		
+	}).catch((e) => {
+		res.status(400).send();
+	});
+});
 
 app.listen(8000, () => {
 	console.log('Started on port 8000');
